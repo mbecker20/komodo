bcf2aada0bee4e81cadef699851abbdc74c49d8a
mbecker20 <becker.maxh@gmail.com>
2022-03-20 11:06:41 -0700

    just use node 10 it doesnt work on older versions

--- cli/vite.config.js
+++ cli/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   build: {
     outDir: "build",
-    target: "node10",
+    target: "node12",
     ssr: true,
     rollupOptions: {
       input: "./src/cli.tsx",
