3a04d23116a35c2fba238c7e909b8e0e96c1a501
mbecker20 <becker.maxh@gmail.com>
2022-03-24 21:31:24 -0700

    something

--- frontend/src/components/deployment/tabs/config/Image.tsx
+++ frontend/src/components/deployment/tabs/config/Image.tsx
@@ -1,16 +1,29 @@
 import { Deployment } from "@monitor/types";
 import { Component } from "solid-js";
 import { DeepReadonly, SetStoreFunction } from "solid-js/store";
+import { useToggle } from "../../../../util/hooks";
+import Flex from "../../../util/layout/Flex";
 import Grid from "../../../util/layout/Grid";
+import Menu from "../../../util/menu/Menu";
 import s from "../../deployment.module.css";
 
 const Image: Component<{
   deployment: DeepReadonly<Deployment>;
   setDeployment: SetStoreFunction<Deployment>;
 }> = (p) => {
+  const [show, toggle] = useToggle();
   return (
     <Grid>
       <div class={s.ItemHeader}>image</div>
+      <Flex>
+        <div>select build: </div>
+        <Menu
+          show={show()}
+          target={<div onClick={toggle}>build1</div>}
+          content={<div></div>}
+          position="bottom center"
+        />
+      </Flex>
     </Grid>
   );
 };
