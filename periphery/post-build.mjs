56cba378e5acfc7914b4e0c4f6a10573fadf0352
mbecker20 <becker.maxh@gmail.com>
2022-04-18 18:38:20 -0700

    fix build given util-node dependency

--- periphery/post-build.mjs
+++ periphery/post-build.mjs
@@ -6,5 +6,6 @@ const json = JSON.parse(raw);
 delete json.scripts;
 delete json.devDependencies;
 delete json.dependencies["@monitor/util"];
+delete json.dependencies["@monitor/util-node"];
 
 writeFileSync("./build/package.json", JSON.stringify(json, undefined, 2));
