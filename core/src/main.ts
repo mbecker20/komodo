5d2b10fdb93a411ab4c9c670ff86da91d4c3c7f8
beckerinj <becker@assc.ai>
2022-05-10 13:26:28 -0400

    notify slack on startup

--- core/src/main.ts
+++ core/src/main.ts
@@ -10,6 +10,7 @@ import frontend from "./plugins/frontend";
 import actionStates from "./plugins/actionStates";
 import routes from "./routes";
 import slackNotifier from "./plugins/slackNotifier";
+import { notifySlack } from "./util/slack";
 
 async function main() {
   const app = fastify({ logger: LOGGER })
@@ -41,6 +42,11 @@ async function main() {
       process.exit(1);
     }
     if (!LOGGER) console.log(`monitor core listening at ${address}`);
+    notifySlack(`
+      INFO | monitor core has launched
+
+      connection to slack is enabled
+    `);
   });
 }
 
