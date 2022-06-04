9c37cff49fb4b771fc09d19b08ef528c951a65f4
beckerinj <becker@assc.ai>
2022-06-04 21:10:59 +0300

    try catch jwt verify

--- core/src/plugins/ws.ts
+++ core/src/plugins/ws.ts
@@ -29,38 +29,43 @@ const ws = fp((app: FastifyInstance, _: {}, done: () => void) => {
 
   app.get("/ws", { websocket: true }, async (connection) => {
     connection.socket.on("message", async (msg) => {
-      const jwt = JSON.parse(msg.toString()).token;
-      if (jwt && app.jwt.verify(jwt)) {
-        const payload = decode(jwt) as { id: string };
-        const userID = payload.id;
-        const user = await app.users.findById(userID);
-        if (user && user.enabled) {
-          const unsub = messages.subscribe((msg) =>
-            connection.socket.send(msg)
-          );
-          connection.socket.removeAllListeners("message");
-          connection.socket.on("message", (raw) => {
-            const msg = raw.toString();
-            if (msg === "PING") {
-              connection.socket.send("PONG");
-              return;
-            }
-            handleMessage(
-              app,
-              connection.socket,
-              JSON.parse(msg),
-              userID
+      try {
+        const jwt = JSON.parse(msg.toString()).token;
+        if (jwt && app.jwt.verify(jwt)) {
+          const payload = decode(jwt) as { id: string };
+          const userID = payload.id;
+          const user = await app.users.findById(userID);
+          if (user && user.enabled) {
+            const unsub = messages.subscribe((msg) =>
+              connection.socket.send(msg)
             );
-          });
-          connection.socket.on("close", unsub);
-          connection.socket.send(
-            JSON.stringify({ type: "LOGIN", message: "logged in successfully" })
-          );
+            connection.socket.removeAllListeners("message");
+            connection.socket.on("message", (raw) => {
+              const msg = raw.toString();
+              if (msg === "PING") {
+                connection.socket.send("PONG");
+                return;
+              }
+              handleMessage(
+                app,
+                connection.socket,
+                JSON.parse(msg),
+                userID
+              );
+            });
+            connection.socket.on("close", unsub);
+            connection.socket.send(
+              JSON.stringify({ type: "LOGIN", message: "logged in successfully" })
+            );
+          } else {
+            connection.socket.close();
+          }
         } else {
           connection.socket.close();
         }
-      } else {
-        connection.socket.close();
+      } catch (error) {
+        console.log("WEBSOCKET ERROR");
+        console.log(error);
       }
     });
   });
