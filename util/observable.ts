11f03fbebf69a01801de92110e4083a6a0dfdfa7
beckerinj <becker@assc.ai>
2022-07-09 03:08:37 -0400

    create user observable to filter published message by authorization

--- util/observable.ts
+++ util/observable.ts
@@ -12,3 +12,23 @@ export function createObservable<Message>() {
     },
   };
 }
+
+export function createUserObservable<User = any, Message = any>() {
+  const subscribers = new Set<{ user: User; cb: (message: string) => void }>();
+  return {
+    subscribe: (user: User, callback: (message: string) => void) => {
+      const observer = { user, cb: callback };
+      subscribers.add(observer);
+      return () => {
+        subscribers.delete(observer);
+      };
+    },
+    publish: (msg: Message, userFilter?: (user: User) => boolean) => {
+      if (userFilter) {
+        subscribers.forEach((observer) => {if (userFilter(observer.user)) observer.cb(JSON.stringify(msg))});
+      } else {
+        subscribers.forEach((observer) => observer.cb(JSON.stringify(msg)));
+      }
+    },
+  };
+}
