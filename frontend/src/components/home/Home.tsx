8522ef7edb5ac42e4b68c072fb0fd9e51942566b
beckerinj <becker@assc.ai>
2022-08-07 15:20:08 -0400

    try this

--- frontend/src/components/home/Home.tsx
+++ frontend/src/components/home/Home.tsx
@@ -1,4 +1,4 @@
-import { Component, For, Show } from "solid-js";
+import { Component, For, Match, Show, Switch } from "solid-js";
 import { useAppState } from "../../state/StateProvider";
 import { useTheme } from "../../state/ThemeProvider";
 import { useUser } from "../../state/UserProvider";
@@ -10,9 +10,11 @@ import s from "./home.module.scss";
 import AddServer from "./Tree/AddServer";
 import Summary from "./Summary/Summary";
 import Updates from "./Updates/Updates";
+import { useAppDimensions } from "../../state/DimensionProvider";
 
 const Home: Component<{}> = (p) => {
   const { servers } = useAppState();
+  const { width, isMobile } = useAppDimensions();
   const serverIDs = () => servers.loaded() && servers.ids();
   return (
     <Grid gap="0rem" class={s.Home} placeItems="start center">
@@ -40,6 +42,59 @@ const Home: Component<{}> = (p) => {
         <Updates />
       </Grid>
     </Grid>
+    // <Switch>
+    //   <Match when={width() >= 1200}>
+    //     <Grid gap="0rem" class={s.Home} placeItems="start center">
+    //       <Tabs
+    //         localStorageKey="home-tab"
+    //         containerClass={s.Tabs}
+    //         tabs={[
+    //           {
+    //             title: "deployments",
+    //             element: (
+    //               <Grid>
+    //                 <For each={serverIDs()}>{(id) => <Server id={id} />}</For>
+    //                 <AddServer />
+    //               </Grid>
+    //             ),
+    //           },
+    //           {
+    //             title: "builds",
+    //             element: <Builds />,
+    //           },
+    //         ]}
+    //       />
+    //       <Grid gap="0rem" style={{ width: "80%" }}>
+    //         <Summary />
+    //         <Updates />
+    //       </Grid>
+    //     </Grid>
+    //   </Match>
+    //   <Match when={width() < 1200}>
+    //     <Grid gap="0rem" class={s.Home} placeItems="start center">
+    //       <Summary />
+    //       <Tabs
+    //         localStorageKey="home-tab"
+    //         containerClass={s.Tabs}
+    //         tabs={[
+    //           {
+    //             title: "deployments",
+    //             element: (
+    //               <Grid>
+    //                 <For each={serverIDs()}>{(id) => <Server id={id} />}</For>
+    //                 <AddServer />
+    //               </Grid>
+    //             ),
+    //           },
+    //           {
+    //             title: "builds",
+    //             element: <Builds />,
+    //           },
+    //         ]}
+    //       />
+    //     </Grid>
+    //   </Match>
+    // </Switch>
   );
 };
 
