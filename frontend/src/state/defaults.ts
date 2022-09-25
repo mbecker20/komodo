1852ee52168b9be96ed811a2932d03c3bd6706e1
beckerinj <becker@assc.ai>
2022-09-25 16:59:59 -0400

    delete everything for next version

--- frontend/src/state/defaults.ts
+++
@@ -1,6 +0,0 @@
-export function defaultDeployment(name: string, serverID: string) {
-	return {
-		name,
-		serverID,
-	}
-}
--- util/validate.ts
+++
@@ -1,5 +0,0 @@
-import { Command } from "@monitor/types";
-
-export function isValidCommand({ command }: Command) {
-	return !command.includes("&&");
-}
