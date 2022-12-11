ffeca7658a149fb9356111cb5974791ab691f42f
beckerinj <becker@assc.ai>
2022-12-11 12:29:48 -0500

    fill out monitor client permissions and secret calls

--- lib/monitor_client/src/permissions.rs
+++ lib/monitor_client/src/permissions.rs
@@ -1,1 +1,36 @@
+use monitor_types::{PermissionLevel, PermissionsTarget};
+use serde_json::json;
 
+use crate::MonitorClient;
+
+impl MonitorClient {
+    pub async fn update_user_permissions_on_target(
+        &self,
+        user_id: &str,
+        permission: PermissionLevel,
+        target_type: PermissionsTarget,
+        target_id: &str,
+    ) -> anyhow::Result<String> {
+        self.post(
+            "/api/permissions/update",
+            json!({
+                "user_id": user_id,
+                "permission": permission,
+                "target_type": target_type,
+                "target_id": target_id
+            }),
+        )
+        .await
+    }
+
+    pub async fn modify_user_enabled(&self, user_id: &str, enabled: bool) -> anyhow::Result<()> {
+        self.post(
+            "/api/permissions/update",
+            json!({
+                "user_id": user_id,
+                "enabled": enabled,
+            }),
+        )
+        .await
+    }
+}
