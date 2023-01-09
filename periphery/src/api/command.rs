82f9076f3e60c3a312ab76e3a221b26357c58739
mbecker20 <becker.maxh@gmail.com>
2023-01-09 01:36:12 +0000

    spawn command handler

--- periphery/src/api/command.rs
+++ periphery/src/api/command.rs
@@ -1,19 +1,25 @@
+use anyhow::Context;
 use axum::{routing::post, Json, Router};
-use helpers::run_monitor_command;
+use helpers::{handle_anyhow_error, run_monitor_command};
 use types::Command;
 
 pub fn router() -> Router {
     Router::new().route(
         "/",
         post(|Json(Command { path, command })| async move {
-            let command = if path.is_empty() {
-                command
-            } else {
-                let path = path.replace("~", &std::env::var("HOME").unwrap());
-                format!("cd {path} && {command}")
-            };
-            let log = run_monitor_command("run command", command).await;
-            Json(log)
+            tokio::spawn(async move {
+                let command = if path.is_empty() {
+                    command
+                } else {
+                    let path = path.replace("~", &std::env::var("HOME").unwrap());
+                    format!("cd {path} && {command}")
+                };
+                let log = run_monitor_command("run command", command).await;
+                Json(log)
+            })
+            .await
+            .context("failure in spawned task")
+            .map_err(handle_anyhow_error)
         }),
     )
 }
