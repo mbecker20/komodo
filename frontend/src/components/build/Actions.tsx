5d0563968bcacb80a4e3d46fab8819a37dd5e0e6
mbecker20 <becker.maxh@gmail.com>
2023-01-16 02:33:57 +0000

    fix build actions height

--- frontend/src/components/build/Actions.tsx
+++ frontend/src/components/build/Actions.tsx
@@ -20,52 +20,54 @@ const Actions: Component<{}> = (p) => {
   const actions = useActionStates();
   const userCanExecute = () =>
     user().admin ||
-    build().permissions[getId(user())] === PermissionLevel.Execute ||
-    build().permissions[getId(user())] === PermissionLevel.Execute;
+    build().permissions![getId(user())] === PermissionLevel.Execute ||
+    build().permissions![getId(user())] === PermissionLevel.Execute;
   return (
     <Show when={userCanExecute()}>
-      <Grid class={combineClasses("card shadow")}>
+      <Grid class={combineClasses("card shadow")} gridTemplateRows="auto 1fr">
         <h1>actions</h1>
-        <Flex class={combineClasses("action shadow")}>
-          build{" "}
-          <Show
-            when={!actions.building}
-            fallback={
-              <button class="green">
-                <Loading type="spinner" />
-              </button>
-            }
-          >
-            <ConfirmButton
-              class="green"
-              onConfirm={() => {
-                client.build(params.id);
-              }}
+        <Grid style={{ height: "fit-content" }}>
+          <Flex class={combineClasses("action shadow")}>
+            build{" "}
+            <Show
+              when={!actions.building}
+              fallback={
+                <button class="green">
+                  <Loading type="spinner" />
+                </button>
+              }
             >
-              <Icon type="build" />
-            </ConfirmButton>
-          </Show>
-        </Flex>
-        <Flex class={combineClasses("action shadow")}>
-          reclone{" "}
-          <Show
-            when={!actions.recloning}
-            fallback={
-              <button class="orange">
-                <Loading type="spinner" />
-              </button>
-            }
-          >
-            <ConfirmButton
-              class="orange"
-              onConfirm={() => {
-                client.reclone_build(params.id)
-              }}
+              <ConfirmButton
+                class="green"
+                onConfirm={() => {
+                  client.build(params.id);
+                }}
+              >
+                <Icon type="build" />
+              </ConfirmButton>
+            </Show>
+          </Flex>
+          <Flex class={combineClasses("action shadow")}>
+            reclone{" "}
+            <Show
+              when={!actions.recloning}
+              fallback={
+                <button class="orange">
+                  <Loading type="spinner" />
+                </button>
+              }
             >
-              <Icon type="reset" />
-            </ConfirmButton>
-          </Show>
-        </Flex>
+              <ConfirmButton
+                class="orange"
+                onConfirm={() => {
+                  client.reclone_build(params.id);
+                }}
+              >
+                <Icon type="reset" />
+              </ConfirmButton>
+            </Show>
+          </Flex>
+        </Grid>
       </Grid>
     </Show>
   );
