7d9d0a9fc43b68939472250e2dc156643798fa6f
mbecker20 <becker.maxh@gmail.com>
2023-03-05 21:42:11 +0000

    add view of resources you can access on account page

--- frontend/src/components/account/Resources.tsx
+++ frontend/src/components/account/Resources.tsx
@@ -39,14 +39,48 @@ const Resources: Component<{}> = (p) => {
         style={{ width: "100%", "box-sizing": "border-box" }}
       >
         <h1>servers</h1>
-        <For each={_servers()}>
-          {(item) => (
-            <Flex class="card light shadow" justifyContent="space-between">
-              <h2>{item.server.name}</h2>
-              
-            </Flex>
-          )}
-        </For>
+        <Grid gridTemplateColumns={isMobile() ? undefined : "1fr 1fr"}>
+          <For each={_servers()}>
+            {(item) => (
+              <Flex class="card light shadow" justifyContent="space-between">
+                <h2>{item.server.name}</h2>
+                <div>{item.server.permissions?.[user_id()] || "none"}</div>
+              </Flex>
+            )}
+          </For>
+        </Grid>
+      </Grid>
+      <Grid
+        class="card shadow"
+        style={{ width: "100%", "box-sizing": "border-box" }}
+      >
+        <h1>deployments</h1>
+        <Grid gridTemplateColumns={isMobile() ? undefined : "1fr 1fr"}>
+          <For each={_deployments()}>
+            {(item) => (
+              <Flex class="card light shadow" justifyContent="space-between">
+                <h2>{item.deployment.name}</h2>
+                <div>{item.deployment.permissions?.[user_id()] || "none"}</div>
+              </Flex>
+            )}
+          </For>
+        </Grid>
+      </Grid>
+      <Grid
+        class="card shadow"
+        style={{ width: "100%", "box-sizing": "border-box" }}
+      >
+        <h1>builds</h1>
+        <Grid gridTemplateColumns={isMobile() ? undefined : "1fr 1fr"}>
+          <For each={_builds()}>
+            {(item) => (
+              <Flex class="card light shadow" justifyContent="space-between">
+                <h2>{item.name}</h2>
+                <div>{item.permissions?.[user_id()] || "none"}</div>
+              </Flex>
+            )}
+          </For>
+        </Grid>
       </Grid>
     </>
   );
