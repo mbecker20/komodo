abdae988167e0220284787ed43e21634f7ae6a86
mbecker20 <becker.maxh@gmail.com>
2023-05-03 20:02:37 +0000

    core handle term signal

--- core/src/main.rs
+++ core/src/main.rs
@@ -4,6 +4,7 @@ use ::helpers::get_socket_addr;
 use auth::JwtClient;
 use axum::{http::StatusCode, Router};
 use state::State;
+use termination_signal::tokio::immediate_term_handle;
 use tower_http::cors::{Any, CorsLayer};
 
 mod actions;
@@ -22,29 +23,40 @@ type ResponseResult<T> = Result<T, (StatusCode, String)>;
 async fn main() -> anyhow::Result<()> {
     println!("version: v{}", env!("CARGO_PKG_VERSION"));
 
-    let (config, spa_router) = config::load();
+    let term_signal = immediate_term_handle()?;
 
-    println!("starting monitor core on port {}...", config.port);
+    let app = tokio::spawn(async move {
+        let (config, spa_router) = config::load();
 
-    let app = Router::new()
-        .merge(spa_router)
-        .nest("/api", api::router())
-        .nest("/auth", auth::router(&config))
-        .nest("/ws", ws::router())
-        .layer(JwtClient::extension(&config))
-        .layer(State::extension(config.clone()).await)
-        .layer(
-            CorsLayer::new()
-                .allow_origin(Any)
-                .allow_methods(Any)
-                .allow_headers(Any),
-        );
+        println!("starting monitor core on port {}...", config.port);
 
-    println!("started monitor core on port {}", config.port);
+        let app = Router::new()
+            .merge(spa_router)
+            .nest("/api", api::router())
+            .nest("/auth", auth::router(&config))
+            .nest("/ws", ws::router())
+            .layer(JwtClient::extension(&config))
+            .layer(State::extension(config.clone()).await)
+            .layer(
+                CorsLayer::new()
+                    .allow_origin(Any)
+                    .allow_methods(Any)
+                    .allow_headers(Any),
+            );
 
-    axum::Server::bind(&get_socket_addr(config.port))
-        .serve(app.into_make_service())
-        .await?;
+        println!("started monitor core on port {}", config.port);
+
+        axum::Server::bind(&get_socket_addr(config.port))
+            .serve(app.into_make_service())
+            .await?;
+
+        anyhow::Ok(())
+    });
+
+    tokio::select! {
+        res = app => return res?,
+        _ = term_signal => {},
+    }
 
     Ok(())
 }
