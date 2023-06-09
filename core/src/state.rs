d36f399d08ec9feadcf66a8767a547dfe605378b
mbecker20 <becker.maxh@gmail.com>
2023-06-09 15:50:37 +0000

    rename core AppState to State

--- core/src/state.rs
+++ core/src/state.rs
@@ -1,10 +1,10 @@
 use std::sync::Arc;
 
-pub struct AppState {}
+pub struct State {}
 
-impl AppState {
-    pub async fn load() -> anyhow::Result<Arc<AppState>> {
-        let state = AppState {};
+impl State {
+    pub async fn load() -> anyhow::Result<Arc<State>> {
+        let state = State {};
 
         Ok(state.into())
     }
