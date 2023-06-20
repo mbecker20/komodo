893a7f22a41eee17a1e7c65563edafd9e04f1744
mbecker20 <becker.maxh@gmail.com>
2023-06-20 07:15:53 +0000

    refactor api router out of main

--- core/src/main.rs
+++ core/src/main.rs
@@ -1,20 +1,10 @@
 #[macro_use]
 extern crate log;
 
-use std::time::Instant;
-
-use auth::{auth_request, RequestUserExtension};
-use axum::{
-    headers::ContentType, http::StatusCode, middleware, routing::post, Extension, Json, Router,
-    TypedHeader,
-};
-use resolver_api::Resolver;
-use state::StateExtension;
+use axum::{Extension, Router};
 use termination_signal::tokio::immediate_term_handle;
-use uuid::Uuid;
 
-use crate::requests::api::ApiRequest;
-
+mod api;
 mod auth;
 mod config;
 mod db;
@@ -33,7 +23,7 @@ async fn app() -> anyhow::Result<()> {
 
     let app = Router::new()
         .nest("/auth", auth::router(&state))
-        .nest("/api", api())
+        .nest("/api", api::router())
         .nest("/ws", ws::router())
         .layer(Extension(state));
 
@@ -44,35 +34,6 @@ async fn app() -> anyhow::Result<()> {
         .await?;
 
     Ok(())
-}
-
-fn api() -> Router {
-    Router::new()
-        .route(
-            "/",
-            post(
-                |state: StateExtension,
-                 Extension(user): RequestUserExtension,
-                 Json(request): Json<ApiRequest>| async move {
-                    let timer = Instant::now();
-                    let req_id = Uuid::new_v4();
-                    info!("/auth request {req_id} | {request:?}");
-                    let res = state
-                        .resolve_request(request, user)
-                        .await
-                        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, format!("{e:?}")));
-                    if let Err(e) = &res {
-                        info!("/auth request {req_id} ERROR: {e:?}");
-                    }
-                    let res = res?;
-                    let elapsed = timer.elapsed();
-                    info!("/auth request {req_id} | resolve time: {elapsed:?}");
-                    debug!("/auth request {req_id} RESPONSE: {res}");
-                    Result::<_, (StatusCode, String)>::Ok((TypedHeader(ContentType::json()), res))
-                },
-            ),
-        )
-        .layer(middleware::from_fn(auth_request))
 }
 
 #[tokio::main]
