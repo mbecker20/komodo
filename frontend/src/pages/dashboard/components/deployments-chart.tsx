493044f4e434dcfe382c81d33aa5e94f9df2220c
karamvir <karamvir.singh98@gmail.com>
2023-07-31 21:02:20 -0700

    deployments chart uses summary

--- frontend/src/pages/dashboard/components/deployments-chart.tsx
+++ frontend/src/pages/dashboard/components/deployments-chart.tsx
@@ -9,20 +9,9 @@ import { WithLoading } from "@components/util";
 import { PieChart } from "react-minimal-pie-chart";
 import { Link } from "react-router-dom";
 import { useRead } from "@hooks";
-import { DockerContainerState } from "@monitor/client/dist/types";
 
 export const DeploymentsChart = () => {
-  const { data, isLoading, isError } = useRead("ListDeployments", {});
-
-  const running = data?.filter(
-    (d) => d.state === DockerContainerState.Running
-  ).length;
-  const stopped = data?.filter(
-    (d) => d.state === DockerContainerState.Exited
-  ).length;
-  const not_deployed = data?.filter(
-    (d) => d.state === DockerContainerState.Unknown
-  ).length;
+  const summary = useRead("GetDeploymentsSummary", {}).data;
 
   return (
     <Link to="/deployments" className="w-full">
@@ -31,47 +20,63 @@ export const DeploymentsChart = () => {
           <CardTitle>Deployments</CardTitle>
         </CardHeader>
         <CardContent className="flex gap-4 items-center w-full">
-          <WithLoading {...{ isLoading, isError }}>
-            <div className="flex flex-col gap-2 text-muted-foreground w-full">
-              <CardDescription>
-                <span className="text-green-500 font-bold">{running} </span>
-                Running
-              </CardDescription>
-              <CardDescription>
-                <span className="text-red-500 font-bold">{stopped} </span>
-                Stopped
-              </CardDescription>
-              <CardDescription>
-                <span className="text-blue-500 font-bold">{not_deployed} </span>
-                Not Deployed
-              </CardDescription>
-            </div>
-            <div className="flex justify-end items-center w-full">
-              <PieChart
-                className="w-20 h-20"
-                data={[
-                  {
-                    color: "#22C55E",
-                    value: running ?? 0,
-                    title: "deployed",
-                    key: "deployed",
-                  },
-                  {
-                    color: "#EF0044",
-                    value: stopped ?? 0,
-                    title: "stopped",
-                    key: "stopped",
-                  },
-                  {
-                    color: "#3B82F6",
-                    value: not_deployed ?? 0,
-                    title: "not-deployed",
-                    key: "not-deployed",
-                  },
-                ]}
-              />
-            </div>
-          </WithLoading>
+          <div className="flex flex-col gap-2 text-muted-foreground w-full">
+            <CardDescription>
+              <span className="text-green-500 font-bold">
+                {summary?.running}{" "}
+              </span>
+              Running
+            </CardDescription>
+            <CardDescription>
+              <span className="text-red-500 font-bold">
+                {summary?.stopped}{" "}
+              </span>
+              Stopped
+            </CardDescription>
+            <CardDescription>
+              <span className="text-blue-500 font-bold">
+                {summary?.not_deployed}{" "}
+              </span>
+              Not Deployed
+            </CardDescription>
+            <CardDescription>
+              <span className="text-purple-500 font-bold">
+                {summary?.unknown}{" "}
+              </span>
+              Unknown
+            </CardDescription>
+          </div>
+          <div className="flex justify-end items-center w-full">
+            <PieChart
+              className="w-20 h-20"
+              data={[
+                {
+                  color: "#22C55E",
+                  value: summary?.running ?? 0,
+                  title: "deployed",
+                  key: "deployed",
+                },
+                {
+                  color: "#EF0044",
+                  value: summary?.stopped ?? 0,
+                  title: "stopped",
+                  key: "stopped",
+                },
+                {
+                  color: "#3B82F6",
+                  value: summary?.not_deployed ?? 0,
+                  title: "not-deployed",
+                  key: "not-deployed",
+                },
+                {
+                  color: "purple",
+                  value: summary?.unknown ?? 0,
+                  title: "unknown",
+                  key: "unknown",
+                },
+              ]}
+            />
+          </div>
         </CardContent>
       </Card>
     </Link>
