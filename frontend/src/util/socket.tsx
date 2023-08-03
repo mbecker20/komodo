6fbec9fbd796fc3470e299642284266e20a4363c
karamvir <karamvir.singh98@gmail.com>
2023-08-02 23:28:04 -0700

    add event listeners in a useeffect

--- frontend/src/util/socket.tsx
+++ frontend/src/util/socket.tsx
@@ -4,7 +4,7 @@ import { Button } from "@ui/button";
 import { toast } from "@ui/toast/use-toast";
 import { atom, useAtom } from "jotai";
 import { Circle } from "lucide-react";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import rws from "reconnecting-websocket";
 import { cn } from "./helpers";
 import { UPDATE_WS_URL } from "@main";
@@ -32,49 +32,51 @@ export const WebsocketProvider = ({ children }: { children: ReactNode }) => {
   const invalidate = useInvalidate();
   const [ws] = useWebsocket();
 
-  ws.addEventListener("open", () => {
-    const token = localStorage.getItem("monitor-auth-token");
-    if (token) ws.send(token);
-  });
-
-  ws.addEventListener("message", ({ data }) => {
-    if (data == "LOGGED_IN") return console.log("logged in to ws");
-    const update = JSON.parse(data) as Types.Update;
-
-    toast({
-      title: `${update.target} ${update.operation}`,
-      description: update.operator,
+  useEffect(() => {
+    ws.addEventListener("open", () => {
+      const token = localStorage.getItem("monitor-auth-token");
+      if (token) ws.send(token);
     });
 
-    invalidate(["ListUpdates"]);
+    ws.addEventListener("message", ({ data }) => {
+      if (data == "LOGGED_IN") return console.log("logged in to ws");
+      const update = JSON.parse(data) as Types.Update;
 
-    if (update.target.type === "Deployment") {
-      invalidate(
-        ["ListDeployments"],
-        ["GetDeployment", { id: update.target.id }],
-        ["GetLog", { id: update.target.id }],
-        ["GetDeploymentActionState", { id: update.target.id }],
-        ["GetDeploymentStatus", { id: update.target.id }]
-      );
-    }
+      toast({
+        title: `${update.target} ${update.operation}`,
+        description: update.operator,
+      });
 
-    if (update.target.type === "Server") {
-      invalidate(
-        ["ListServers"],
-        ["GetServer", { id: update.target.id }],
-        ["GetServerActionState", { id: update.target.id }],
-        ["GetServerStatus", { id: update.target.id }],
-        ["GetHistoricalServerStats", { id: update.target.id }]
-      );
-    }
+      invalidate(["ListUpdates"]);
 
-    if (update.target.type === "Build") {
-      invalidate(
-        ["ListBuilds"],
-        ["GetBuild", { id: update.target.id }],
-        ["GetBuildActionState", { id: update.target.id }]
-      );
-    }
+      if (update.target.type === "Deployment") {
+        invalidate(
+          ["ListDeployments"],
+          ["GetDeployment", { id: update.target.id }],
+          ["GetLog", { id: update.target.id }],
+          ["GetDeploymentActionState", { id: update.target.id }],
+          ["GetDeploymentStatus", { id: update.target.id }]
+        );
+      }
+
+      if (update.target.type === "Server") {
+        invalidate(
+          ["ListServers"],
+          ["GetServer", { id: update.target.id }],
+          ["GetServerActionState", { id: update.target.id }],
+          ["GetServerStatus", { id: update.target.id }],
+          ["GetHistoricalServerStats", { id: update.target.id }]
+        );
+      }
+
+      if (update.target.type === "Build") {
+        invalidate(
+          ["ListBuilds"],
+          ["GetBuild", { id: update.target.id }],
+          ["GetBuildActionState", { id: update.target.id }]
+        );
+      }
+    });
   });
 
   return <>{children}</>;
