d15607048eee60fced53f938dc4273e831af1dbc
karamvir <karamvir.singh98@gmail.com>
2023-08-02 20:40:07 -0700

    reduce gap

--- frontend/src/layouts/resources.tsx
+++ frontend/src/layouts/resources.tsx
@@ -48,7 +48,7 @@ export const Resources = ({
         </div>
       }
       content={
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
           {components(search)}
         </div>
       }
