6ce14ffc80714daa094e198b3db72b6519f04065
karamvir <karamvir.singh98@gmail.com>
2023-08-03 17:22:51 -0700

    place servers first on dashboard

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -18,11 +18,11 @@ const MyResources = () => (
     <div className="flex flex-col gap-4">
       <div className="flex flex-col lg:flex-row gap-4">
         <div className="flex flex-col md:flex-row gap-4 w-full">
-          <ResourceOverviewCard type="Deployment">
-            <DeploymentsChart />
-          </ResourceOverviewCard>
           <ResourceOverviewCard type="Server">
             <ServersChart />
+          </ResourceOverviewCard>
+          <ResourceOverviewCard type="Deployment">
+            <DeploymentsChart />
           </ResourceOverviewCard>
         </div>
         <div className="w-full lg:max-w-[50%]">
