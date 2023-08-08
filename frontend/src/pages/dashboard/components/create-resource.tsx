f1aca3df4d5c986ccc52a1d1924a3aa171daeaa9
karamvir <karamvir.singh98@gmail.com>
2023-08-08 03:01:29 -0700

    add icons to create resource dropdown

--- frontend/src/pages/dashboard/components/create-resource.tsx
+++ frontend/src/pages/dashboard/components/create-resource.tsx
@@ -10,10 +10,28 @@ import {
   DropdownMenuItem,
 } from "@ui/dropdown";
 import { Button } from "@ui/button";
-import { PlusCircle, ChevronDown } from "lucide-react";
+import {
+  PlusCircle,
+  ChevronDown,
+  Rocket,
+  Server,
+  Hammer,
+  Factory,
+  AlarmClock,
+  GitBranch,
+} from "lucide-react";
 import { useState } from "react";
 import { RESOURCE_TYPES } from "@util/config";
 import { NewResourceDialog } from "@layouts/resources";
+
+const Icons: { [key in (typeof RESOURCE_TYPES)[number]]: JSX.Element } = {
+  Deployment: <Rocket className="w-4 h-4" />,
+  Server: <Server className="w-4 h-4" />,
+  Build: <Hammer className="w-4 h-4" />,
+  Builder: <Factory className="w-4 h-4" />,
+  Alerter: <AlarmClock className="w-4 h-4" />,
+  Repo: <GitBranch className="w-4 h-4" />,
+};
 
 export const CreateResource = () => {
   const [open, set] = useState<Types.ResourceTarget["type"] | false>(false);
@@ -41,10 +59,11 @@ export const CreateResource = () => {
           <DropdownMenuGroup>
             {RESOURCE_TYPES.map((target) => (
               <DropdownMenuItem
-                className="cursor-pointer"
+                className="cursor-pointer flex items-center gap-2"
                 onClick={() => set(target as ResourceTarget["type"])}
                 key={target}
               >
+                {Icons[target]}
                 {target}
               </DropdownMenuItem>
             ))}
--- frontend/src/util/config.ts
+++ frontend/src/util/config.ts
@@ -1,10 +1,4 @@
 import { Types } from "@monitor/client";
 
-export const RESOURCE_TYPES: Types.ResourceTarget["type"][] = [
-  "Alerter",
-  "Build",
-  "Builder",
-  "Deployment",
-  "Repo",
-  "Server",
-];
+export const RESOURCE_TYPES: Exclude<Types.ResourceTarget["type"], "System">[] =
+  ["Alerter", "Build", "Builder", "Deployment", "Repo", "Server"];
