0ff9a7e50d2ae5d7f6d3ddd7cf51db68a5f4887c
mbecker20 <becker.maxh@gmail.com>
2023-08-10 01:46:07 -0400

    load migrator config

--- bin/migrator/src/main.rs
+++ bin/migrator/src/main.rs
@@ -1,6 +1,18 @@
+use crate::config::State;
+
+#[macro_use]
+extern crate log;
+
+mod config;
 mod legacy;
 
 #[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> anyhow::Result<()> {
+    logger::init(log::LevelFilter::Info)?;
+
+    info!("starting migrator");
+
+    let state = State::load().await?;
+
+    Ok(())
 }
