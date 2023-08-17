76d331ef03585e22f88173d55e7b2fafcfdc3efb
karamvir <karamvir.singh98@gmail.com>
2023-08-17 15:38:02 -0700

    re-cast to any to ensure build

--- frontend/src/components/config/util.tsx
+++ frontend/src/components/config/util.tsx
@@ -102,13 +102,13 @@ export const DoubleInput = <
       {values?.map((value, i) => (
         <div className="flex items-center justify-between gap-4" key={i}>
           <Input
-            value={value[leftval]}
+            value={value[leftval] as any}
             placeholder={leftpl}
             onChange={(e) => onLeftChange(e.target.value as T[L], i)}
           />
           :
           <Input
-            value={value[rightval]}
+            value={value[rightval] as any}
             placeholder={rightpl}
             onChange={(e) => onRightChange(e.target.value as T[R], i)}
           />
