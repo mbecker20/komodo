2982ac051fb241443aa2d900a9abfc82440fae34
karamvir <karamvir.singh98@gmail.com>
2023-08-17 10:34:25 -0700

    cleanup main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { MonitorClient } from "@monitor/client";
-// import { monitor_client } from "@util/client.ts";
 
 export const MONITOR_BASE_URL =
   import.meta.env.VITE_MONITOR_HOST ?? "https://monitor.v1.api.mogh.tech";
@@ -11,12 +10,6 @@ export const MONITOR_BASE_URL =
 export const UPDATE_WS_URL =
   MONITOR_BASE_URL.replace("http", "ws") + "/ws/update";
 
-// const token =
-//   (import.meta.env.VITE_ACCESS_TOKEN as string) ||
-//   localStorage.getItem("access_token") ||
-//   null;
-
-// export const client = monitor_client(MONITOR_BASE_URL, token);
 const token = localStorage.getItem("monitor-auth-token");
 export const client = MonitorClient(MONITOR_BASE_URL, token ?? undefined);
 const queryClient = new QueryClient({
