c88e2101360da9002846c0b80e5caf8ae921cf1b
karamvir <karamvir.singh98@gmail.com>
2023-09-06 20:36:18 -0700

    init new shadcn cli

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,12 @@
-import animate from "tailwindcss-animate";
-
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   darkMode: ["class"],
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
   theme: {
     container: {
       center: true,
@@ -49,12 +52,9 @@ export default {
         },
       },
       borderRadius: {
-        lg: `var(--radius)`,
-        md: `calc(var(--radius) - 2px)`,
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
-      },
-      fontFamily: {
-        sans: ["Inter"],
       },
       keyframes: {
         "accordion-down": {
@@ -72,5 +72,5 @@ export default {
       },
     },
   },
-  plugins: [animate],
-};
+  plugins: [require("tailwindcss-animate")],
+}
