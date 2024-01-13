5d02a8874fdf061d073c3fae374654be72062fe5
mbecker20 <becker.maxh@gmail.com>
2024-01-12 23:49:50 -0800

    client ws again

--- client/rs/src/ws.rs
+++ client/rs/src/ws.rs
@@ -1,6 +1,16 @@
+use std::time::Duration;
+
 use anyhow::Context;
+use futures::{SinkExt, TryStreamExt};
 use serde::{Deserialize, Serialize};
+use serror::serialize_error;
+use thiserror::Error;
+use tokio::sync::broadcast;
+use tokio_tungstenite::{connect_async, tungstenite::Message};
+use tokio_util::sync::CancellationToken;
 use typeshare::typeshare;
+
+use crate::{entities::update::UpdateListItem, MonitorClient};
 
 #[typeshare]
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -22,178 +32,173 @@ impl WsLoginMessage {
   }
 }
 
-// use std::time::Duration;
+#[derive(Debug, Clone)]
+pub enum UpdateWsMessage {
+  Update(UpdateListItem),
+  Error(UpdateWsError),
+  Disconnected,
+  Reconnected,
+}
 
-// use anyhow::Context;
-// use futures::{SinkExt, TryStreamExt};
-// use serror::serialize_error;
-// use thiserror::Error;
-// use tokio::sync::broadcast;
-// use tokio_tungstenite::{connect_async, tungstenite::Message};
-// use tokio_util::sync::CancellationToken;
+#[derive(Error, Debug, Clone)]
+pub enum UpdateWsError {
+  #[error("failed to connect | {0}")]
+  ConnectionError(String),
+  #[error("failed to login | {0}")]
+  LoginError(String),
+  #[error("failed to recieve message | {0}")]
+  MessageError(String),
+  #[error("did not recognize message | {0}")]
+  MessageUnrecognized(String),
+}
 
-// use crate::{entities::update::UpdateListItem, MonitorClient};
+impl MonitorClient {
+  pub fn subscribe_to_updates(
+    &self,
+    capacity: usize,
+    retry_cooldown_secs: u64,
+  ) -> anyhow::Result<(
+    broadcast::Receiver<UpdateWsMessage>,
+    CancellationToken,
+  )> {
+    let (tx, rx) = broadcast::channel(capacity);
+    let cancel = CancellationToken::new();
+    let cancel_clone = cancel.clone();
+    let address =
+      format!("{}/ws/update", self.address.replacen("http", "ws", 1));
+    let login_msg = WsLoginMessage::ApiKeys {
+      key: self.key.clone(),
+      secret: self.secret.clone(),
+    }
+    .to_json_string()?;
 
-// #[derive(Debug, Clone)]
-// pub enum UpdateWsMessage {
-//   Update(UpdateListItem),
-//   Error(UpdateWsError),
-//   Disconnected,
-//   Reconnected,
-// }
+    tokio::spawn(async move {
+      loop {
+        // OUTER LOOP (LONG RECONNECT)
+        if cancel.is_cancelled() {
+          break;
+        }
+        loop {
+          // INNER LOOP (SHORT RECONNECT)
+          if cancel.is_cancelled() {
+            break;
+          }
 
-// #[derive(Error, Debug, Clone)]
-// pub enum UpdateWsError {
-//   #[error("failed to connect | {0}")]
-//   ConnectionError(String),
-//   #[error("failed to login | {0}")]
-//   LoginError(String),
-//   #[error("failed to recieve message | {0}")]
-//   MessageError(String),
-//   #[error("did not recognize message | {0}")]
-//   MessageUnrecognized(String),
-// }
+          let res = connect_async(&address)
+            .await
+            .context("failed to connect to websocket endpoint");
 
-// impl MonitorClient {
-//   pub fn subscribe_to_updates(
-//     &self,
-//     capacity: usize,
-//     retry_cooldown_secs: u64,
-//   ) -> (broadcast::Receiver<UpdateWsMessage>, CancellationToken) {
-//     let (tx, rx) = broadcast::channel(capacity);
-//     let cancel = CancellationToken::new();
-//     let cancel_clone = cancel.clone();
-//     let address =
-//       format!("{}/ws/update", self.address.replacen("http", "ws", 1));
-//     let mut client = self.clone();
+          if let Err(e) = res {
+            let _ = tx.send(UpdateWsMessage::Error(
+              UpdateWsError::ConnectionError(serialize_error(e)),
+            ));
+            break;
+          }
 
-//     tokio::spawn(async move {
-//       loop {
-//         // OUTER LOOP (LONG RECONNECT)
-//         if cancel.is_cancelled() {
-//           break;
-//         }
-//         loop {
-//           // INNER LOOP (SHORT RECONNECT)
-//           if cancel.is_cancelled() {
-//             break;
-//           }
+          let (mut ws, _) = res.unwrap();
 
-//           let res = connect_async(&address)
-//             .await
-//             .context("failed to connect to websocket endpoint");
+          // ==================
+          // SEND LOGIN MSG
+          // ==================
+          let login_send_res = ws
+            .send(Message::Text(login_msg.clone()))
+            .await
+            .context("failed to send login message");
 
-//           if let Err(e) = res {
-//             let _ = tx.send(UpdateWsMessage::Error(
-//               UpdateWsError::ConnectionError(serialize_error(e)),
-//             ));
-//             break;
-//           }
+          if let Err(e) = login_send_res {
+            let _ = tx.send(UpdateWsMessage::Error(
+              UpdateWsError::LoginError(serialize_error(e)),
+            ));
+            break;
+          }
 
-//           let (mut ws, _) = res.unwrap();
+          // ==================
+          // HANDLE LOGIN RES
+          // ==================
+          match ws.try_next().await {
+            Ok(Some(Message::Text(msg))) => {
+              if msg != "LOGGED_IN" {
+                let _ = tx.send(UpdateWsMessage::Error(
+                  UpdateWsError::LoginError(msg),
+                ));
+                let _ = ws.close(None).await;
+                break;
+              }
+            }
+            Ok(Some(msg)) => {
+              let _ = tx.send(UpdateWsMessage::Error(
+                UpdateWsError::LoginError(format!("{msg:#?}")),
+              ));
+              let _ = ws.close(None).await;
+              break;
+            }
+            Ok(None) => {
+              let _ = tx.send(UpdateWsMessage::Error(
+                UpdateWsError::LoginError(String::from(
+                  "got None message after login message",
+                )),
+              ));
+              let _ = ws.close(None).await;
+              break;
+            }
+            Err(e) => {
+              let _ = tx.send(UpdateWsMessage::Error(
+                UpdateWsError::LoginError(format!(
+                  "failed to recieve message | {e:#?}"
+                )),
+              ));
+              let _ = ws.close(None).await;
+              break;
+            }
+          }
 
-//           // ==================
-//           // SEND LOGIN MSG
-//           // ==================
-//           let login_send_res = ws
-//             .send(Message::Text(client.jwt.clone()))
-//             .await
-//             .context("failed to send login message");
+          let _ = tx.send(UpdateWsMessage::Reconnected);
 
-//           if let Err(e) = login_send_res {
-//             let _ = tx.send(UpdateWsMessage::Error(
-//               UpdateWsError::LoginError(serialize_error(e)),
-//             ));
-//             break;
-//           }
+          // ==================
+          // HANLDE MSGS
+          // ==================
+          loop {
+            match ws
+              .try_next()
+              .await
+              .context("failed to recieve message")
+            {
+              Ok(Some(Message::Text(msg))) => {
+                match serde_json::from_str::<UpdateListItem>(&msg) {
+                  Ok(msg) => {
+                    let _ = tx.send(UpdateWsMessage::Update(msg));
+                  }
+                  Err(_) => {
+                    let _ = tx.send(UpdateWsMessage::Error(
+                      UpdateWsError::MessageUnrecognized(msg),
+                    ));
+                  }
+                }
+              }
+              Ok(Some(Message::Close(_))) => {
+                let _ = tx.send(UpdateWsMessage::Disconnected);
+                let _ = ws.close(None).await;
+                break;
+              }
+              Err(e) => {
+                let _ = tx.send(UpdateWsMessage::Error(
+                  UpdateWsError::MessageError(serialize_error(e)),
+                ));
+                let _ = tx.send(UpdateWsMessage::Disconnected);
+                let _ = ws.close(None).await;
+                break;
+              }
+              Ok(_) => {
+                // ignore
+              }
+            }
+          }
+        }
+        tokio::time::sleep(Duration::from_secs(retry_cooldown_secs))
+          .await;
+      }
+    });
 
-//           // ==================
-//           // HANDLE LOGIN RES
-//           // ==================
-//           match ws.try_next().await {
-//             Ok(Some(Message::Text(msg))) => {
-//               if msg != "LOGGED_IN" {
-//                 let _ = tx.send(UpdateWsMessage::Error(
-//                   UpdateWsError::LoginError(msg),
-//                 ));
-//                 let _ = ws.close(None).await;
-//                 break;
-//               }
-//             }
-//             Ok(Some(msg)) => {
-//               let _ = tx.send(UpdateWsMessage::Error(
-//                 UpdateWsError::LoginError(format!("{msg:#?}")),
-//               ));
-//               let _ = ws.close(None).await;
-//               break;
-//             }
-//             Ok(None) => {
-//               let _ = tx.send(UpdateWsMessage::Error(
-//                 UpdateWsError::LoginError(String::from(
-//                   "got None message after login message",
-//                 )),
-//               ));
-//               let _ = ws.close(None).await;
-//               break;
-//             }
-//             Err(e) => {
-//               let _ = tx.send(UpdateWsMessage::Error(
-//                 UpdateWsError::LoginError(format!(
-//                   "failed to recieve message | {e:#?}"
-//                 )),
-//               ));
-//               let _ = ws.close(None).await;
-//               break;
-//             }
-//           }
-
-//           let _ = tx.send(UpdateWsMessage::Reconnected);
-
-//           // ==================
-//           // HANLDE MSGS
-//           // ==================
-//           loop {
-//             match ws
-//               .try_next()
-//               .await
-//               .context("failed to recieve message")
-//             {
-//               Ok(Some(Message::Text(msg))) => {
-//                 match serde_json::from_str::<UpdateListItem>(&msg) {
-//                   Ok(msg) => {
-//                     let _ = tx.send(UpdateWsMessage::Update(msg));
-//                   }
-//                   Err(_) => {
-//                     let _ = tx.send(UpdateWsMessage::Error(
-//                       UpdateWsError::MessageUnrecognized(msg),
-//                     ));
-//                   }
-//                 }
-//               }
-//               Ok(Some(Message::Close(_))) => {
-//                 let _ = tx.send(UpdateWsMessage::Disconnected);
-//                 let _ = ws.close(None).await;
-//                 break;
-//               }
-//               Err(e) => {
-//                 let _ = tx.send(UpdateWsMessage::Error(
-//                   UpdateWsError::MessageError(serialize_error(e)),
-//                 ));
-//                 let _ = tx.send(UpdateWsMessage::Disconnected);
-//                 let _ = ws.close(None).await;
-//                 break;
-//               }
-//               Ok(_) => {
-//                 // ignore
-//               }
-//             }
-//           }
-//         }
-//         tokio::time::sleep(Duration::from_secs(retry_cooldown_secs))
-//           .await;
-//       }
-//     });
-
-//     (rx, cancel_clone)
-//   }
-// }
+    Ok((rx, cancel_clone))
+  }
+}
