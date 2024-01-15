f97e48e8861c1c8078af7d0384a8c1f352ef1dd5
mbecker20 <becker.maxh@gmail.com>
2024-01-15 01:03:13 -0800

    comment out recently viewed

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -1,28 +1,28 @@
 import { useRead } from "@lib/hooks";
-import { Page, Section, ResourceCard } from "@components/layouts";
-import { Box, History } from "lucide-react";
+import { Page, Section } from "@components/layouts";
+import { Box } from "lucide-react";
 import { DeploymentsChart } from "@components/dashboard/deployments-chart";
 import { ServersChart } from "@components/dashboard/servers-chart";
 import { BuildChart } from "@components/dashboard/builds-chart";
 
-const RecentlyViewed = () => (
-  <Section
-    title="Recently Viewed"
-    icon={<History className="w-4 h-4" />}
-    actions=""
-  >
-    <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {useRead("GetUser", {})
-        .data?.recently_viewed?.slice(0, 6)
-        .map(
-          (target) =>
-            target.type !== "System" && (
-              <ResourceCard target={target} key={target.id} />
-            )
-        )}
-    </div>
-  </Section>
-);
+// const RecentlyViewed = () => (
+//   <Section
+//     title="Recently Viewed"
+//     icon={<History className="w-4 h-4" />}
+//     actions=""
+//   >
+//     <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+//       {useRead("GetUser", {})
+//         .data?.recently_viewed?.slice(0, 6)
+//         .map(
+//           (target) =>
+//             target.type !== "System" && (
+//               <ResourceCard target={target} key={target.id} />
+//             )
+//         )}
+//     </div>
+//   </Section>
+// );
 
 const MyResources = () => (
   <Section title="My Resources" icon={<Box className="w-4 h-4" />} actions="">
