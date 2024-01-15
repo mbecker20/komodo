5893fdefaf1e7fbc2b21f651a60f949774362210
mbecker20 <becker.maxh@gmail.com>
2024-01-15 01:57:11 -0800

    remove line from log

--- lib/logger/src/lib.rs
+++ lib/logger/src/lib.rs
@@ -2,8 +2,6 @@ use tracing::level_filters::LevelFilter;
 
 pub fn init(log_level: tracing::Level) {
   tracing_subscriber::fmt()
-    .with_level(true)
-    .with_line_number(true)
     .with_max_level(LevelFilter::from(log_level))
     .init()
 }
