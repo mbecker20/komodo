c9334074f69528a156d968b2af4b9a0049921774
mbecker20 <becker.maxh@gmail.com>
2024-03-25 16:34:15 -0700

    fix show updates

--- frontend/src/components/updates/table.tsx
+++ frontend/src/components/updates/table.tsx
@@ -1,44 +1,51 @@
 import { fmt_date_with_minutes } from "@lib/utils";
 import { Types } from "@monitor/client";
+import { ColumnDef } from "@tanstack/react-table";
 import { DataTable } from "@ui/data-table";
 
 export const UpdatesTable = ({
   updates,
-	showTarget,
+  showTarget,
 }: {
   updates: Types.UpdateListItem[];
 	showTarget?: boolean;
 }) => {
+  let data: ColumnDef<Types.UpdateListItem, string>[] = [
+    {
+      header: "Operation",
+      accessorKey: "operation",
+    },
+    {
+      header: "Status",
+      accessorKey: "status",
+    },
+    {
+      header: "Success",
+      accessorFn: ({ success }) => (success ? "Ok" : "Fail"),
+    },
+    {
+      header: "Start Time",
+      accessorFn: ({ start_ts }) => fmt_date_with_minutes(new Date(start_ts)),
+    },
+    {
+      header: "Operator",
+      accessorKey: "username",
+    },
+  ];
+  // attach the target column on front
+  if (showTarget) {
+    data = [
+      {
+        header: "Target",
+        accessorKey: "target.id",
+      },
+      ...data
+    ];
+  }
   return (
     <DataTable
       data={updates}
-      columns={[
-        // showTarget ? {
-        // 	header: "Target",
-        // 	accessorKey: "target.id",
-        // } : undefined,
-        {
-          header: "Operation",
-          accessorKey: "operation",
-        },
-        {
-          header: "Status",
-          accessorKey: "status",
-        },
-        {
-          header: "Success",
-          accessorFn: ({ success }) => success ? "Ok" : "Fail",
-        },
-        {
-          header: "Start Time",
-          accessorFn: ({ start_ts }) =>
-            fmt_date_with_minutes(new Date(start_ts)),
-        },
-        {
-          header: "Operator",
-          accessorKey: "username",
-        },
-      ]}
+      columns={data}
     />
   );
 };
