57a3561aa8e0d16774a81c3b76141ce40490a1bb
mbecker20 <becker.maxh@gmail.com>
2024-04-04 01:45:10 -0700

    finish build config

--- frontend/src/components/resources/build/config.tsx
+++ frontend/src/components/resources/build/config.tsx
@@ -7,12 +7,22 @@ import {
 import { useRead, useWrite } from "@lib/hooks";
 import { env_to_text, text_to_env } from "@lib/utils";
 import { Types } from "@monitor/client";
+import { Button } from "@ui/button";
+import { Input } from "@ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@ui/select";
 import { Textarea } from "@ui/textarea";
+import { MinusCircle, PlusCircle } from "lucide-react";
 import { useEffect, useState } from "react";
 
 export const BuildConfig = ({ id }: { id: string }) => {
   const config = useRead("GetBuild", { build: id }).data?.config;
-  // const orgs = useRead("GetAccounts")
+  const docker_organizations = useRead("ListDockerOrganizations", {}).data;
   const [update, set] = useState<Partial<Types.BuildConfig>>({});
   const { mutate } = useWrite("UpdateBuild");
 
@@ -63,8 +73,19 @@ export const BuildConfig = ({ id }: { id: string }) => {
                 onSelect={(docker_account) => set({ docker_account })}
               />
             ),
+            docker_organization:
+              docker_organizations === undefined ||
+              docker_organizations.length === 0
+                ? undefined
+                : (value, set) => <DockerOrganizations value={value} set={set} />,
             use_buildx: true,
             // docker_organization,
+            extra_args: (value, set) => (
+              <ExtraArgs args={value ?? []} set={set} />
+            ),
+          },
+          pre_build: {
+            pre_build: (value, set) => <PreBuild value={value} set={set} />,
           },
         },
         "Build Args": {
@@ -100,6 +121,118 @@ const BuildArgs = ({
         value={args}
         onChange={(e) => setArgs(e.target.value)}
       />
+    </ConfigItem>
+  );
+};
+
+const ExtraArgs = ({
+  args,
+  set,
+}: {
+  args: string[];
+  set: (update: Partial<Types.BuildConfig>) => void;
+}) => {
+  return (
+    <ConfigItem label="Extra Args" className="items-start">
+      <div className="flex flex-col gap-4 w-full max-w-[400px]">
+        {args.map((arg, i) => (
+          <div className="w-full flex gap-4" key={i}>
+            <Input
+              value={arg}
+              placeholder="--extra-arg=value"
+              onChange={(e) => {
+                args[i] = e.target.value;
+                set({ extra_args: [...args] });
+              }}
+            />
+            <Button
+              variant="outline"
+              onClick={() =>
+                set({ extra_args: [...args.filter((_, idx) => idx !== i)] })
+              }
+            >
+              <MinusCircle className="w-4 h-4" />
+            </Button>
+          </div>
+        ))}
+
+        <Button
+          variant="outline"
+          className="flex items-center gap-2 w-[200px] place-self-end"
+          onClick={() => set({ extra_args: [...args, ""] })}
+        >
+          <PlusCircle className="w-4 h-4" /> Add Extra Arg
+        </Button>
+      </div>
+    </ConfigItem>
+  );
+};
+
+const PreBuild = ({
+  value,
+  set,
+}: {
+  value?: Types.SystemCommand;
+  set: (input: Partial<Types.BuildConfig>) => void;
+}) => {
+  return (
+    <ConfigItem label="Pre Build" className="items-start">
+      <div className="grid gap-2">
+        <div className="flex gap-4 items-center justify-end">
+          Path:
+          <Input
+            placeholder="command working directory"
+            value={value?.path}
+            className="w-[300px]"
+            onChange={(e) =>
+              set({ pre_build: { ...(value || {}), path: e.target.value } })
+            }
+          />
+        </div>
+        <div className="flex gap-4 items-center justify-end">
+          Command:
+          <Input
+            placeholder="shell command"
+            value={value?.command}
+            className="w-[300px]"
+            onChange={(e) =>
+              set({ pre_build: { ...(value || {}), command: e.target.value } })
+            }
+          />
+        </div>
+      </div>
+    </ConfigItem>
+  );
+};
+
+const DockerOrganizations = ({
+  value,
+  set,
+}: {
+  value?: string;
+  set: (input: Partial<Types.BuildConfig>) => void;
+}) => {
+  const docker_organizations = useRead("ListDockerOrganizations", {}).data;
+  return (
+    <ConfigItem label="Docker Organization">
+      <Select
+        value={value}
+        onValueChange={(value) => set({ docker_organization: value })}
+      >
+        <SelectTrigger
+          className="w-full lg:w-[300px] max-w-[50%]"
+        >
+          <SelectValue placeholder="Select Organization" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value={""}>None</SelectItem>
+          {docker_organizations?.map((org) => (
+            <SelectItem key={org} value={org}>
+              {org}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
     </ConfigItem>
   );
 };
