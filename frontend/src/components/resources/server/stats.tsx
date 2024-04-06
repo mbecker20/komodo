b864b32cb2d84d35e897e9ad1429fb32e9b10cf4
mbecker20 <becker.maxh@gmail.com>
2024-04-06 03:58:40 -0700

    basic server stats

--- frontend/src/components/resources/server/stats.tsx
+++ frontend/src/components/resources/server/stats.tsx
@@ -1,4 +1,4 @@
-import { Page } from "@components/layouts";
+import { Page, Section } from "@components/layouts";
 import {
   Card,
   CardContent,
@@ -12,25 +12,99 @@ import { useRead } from "@lib/hooks";
 import { Types } from "@monitor/client";
 import { useServer } from ".";
 import { ServerInfo } from "./info";
+import { DataTable } from "@ui/data-table";
 
 export const ServerStats = ({ id }: { id: string }) => {
   const server = useServer(id);
   const stats = useRead(
     "GetAllSystemStats",
     { server: id },
-    { refetchInterval: 1000 }
+    { refetchInterval: 5000 }
   ).data;
+  const info = useRead("GetSystemInformation", { server: id }).data;
 
   return (
-    <Page title={`${server?.name} Stats`}>
+    <Page
+      title={server?.name}
+      titleRight={<div className="text-muted-foreground">Stats</div>}
+    >
       <div className="flex gap-4">
         <ServerInfo id={id} />
       </div>
-      <div className="flex flex-col lg:flex-row gap-4">
-        <CPU stats={stats} />
-        <RAM stats={stats} />
-        <DISK stats={stats} />
-      </div>
+      <Section title="System Info">
+        <DataTable
+          data={info && [info]}
+          columns={[
+            {
+              header: "Hostname",
+              accessorKey: "host_name",
+            },
+            {
+              header: "Os",
+              accessorKey: "os",
+            },
+            {
+              header: "Kernel",
+              accessorKey: "kernel",
+            },
+          ]}
+        />
+      </Section>
+      <Section title="Basic">
+        <div className="flex flex-col lg:flex-row gap-4">
+          <CPU stats={stats} />
+          <RAM stats={stats} />
+          <DISK stats={stats} />
+        </div>
+      </Section>
+      <Section title="Disks">
+        <DataTable
+          data={stats?.disk.disks}
+          columns={[
+            {
+              header: "Path",
+              accessorKey: "mount",
+            },
+            {
+              header: "Used",
+              accessorFn: (disk) => disk.used_gb.toFixed(2) + " GB",
+            },
+            {
+              header: "Total",
+              accessorFn: (disk) => disk.total_gb.toFixed(2) + " GB",
+            },
+            {
+              header: "Percentage",
+              accessorFn: (disk) =>
+                (100 * (disk.used_gb / disk.total_gb)).toFixed(2) + "% Full",
+            },
+          ]}
+        />
+      </Section>
+      {stats?.processes && stats.processes.length > 0 && (
+        <Section title="Processes">
+          <DataTable
+            data={stats.processes}
+            columns={[
+              {
+                header: "Name",
+                accessorKey: "name",
+              },
+              {
+                header: "Cpu",
+                accessorFn: (process) => `${process.cpu_perc.toFixed(2)} %`,
+              },
+              {
+                header: "Memory",
+                accessorFn: (process) =>
+                  process.mem_mb > 1000
+                    ? `${(process.mem_mb / 1024).toFixed(2)} GB`
+                    : `${process.mem_mb.toFixed(2)} MB`,
+              },
+            ]}
+          />
+        </Section>
+      )}
     </Page>
   );
 };
