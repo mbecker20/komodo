e710317cac824a93d6f750a708a4fe6249b0f008
mbecker20 <becker.maxh@gmail.com>
2024-04-07 02:54:49 -0700

    deployment log show ansi colors

--- frontend/src/components/resources/deployment/logs.tsx
+++ frontend/src/components/resources/deployment/logs.tsx
@@ -19,52 +19,28 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@ui/select";
-
-const to_bottom = (id: string) => () =>
-  document
-    .getElementById(id)
-    ?.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
-
-const TailLengthSelector = ({
-  selected,
-  onSelect,
-}: {
-  selected: string;
-  onSelect: (value: string) => void;
-}) => (
-  <Select value={selected} onValueChange={onSelect}>
-    <SelectTrigger>
-      <SelectValue />
-    </SelectTrigger>
-    <SelectContent>
-      <SelectGroup>
-        {["50", "100", "500", "1000"].map((length) => (
-          <SelectItem key={length} value={length}>
-            {length} lines
-          </SelectItem>
-        ))}
-      </SelectGroup>
-    </SelectContent>
-  </Select>
-);
+import sanitizeHtml from "sanitize-html";
+import Convert from "ansi-to-html";
 
 export const DeploymentLogs = ({ id }: { id: string }) => {
-  const [tail, set] = useState("50");
+  const state = useDeployment(id)?.info.state;
+  if (
+    state === undefined ||
+    state === Types.DockerContainerState.Unknown ||
+    state === Types.DockerContainerState.NotDeployed
+  ) {
+    return null;
+  }
+  return <DeploymentLogsInner id={id} />;
+};
 
+const DeploymentLogsInner = ({ id }: { id: string }) => {
+  const [tail, set] = useState("50");
   const { data: logs, refetch } = useRead(
     "GetLog",
     { deployment: id, tail: Number(tail) },
     { refetchInterval: 30000 }
   );
-
-  const deployment = useDeployment(id);
-
-  if (
-    deployment?.info.state === Types.DockerContainerState.Unknown ||
-    deployment?.info.state === Types.DockerContainerState.NotDeployed
-  )
-    return null;
-
   return (
     <Tabs defaultValue="stdout">
       <Section
@@ -90,22 +66,74 @@ export const DeploymentLogs = ({ id }: { id: string }) => {
           </div>
         }
       >
-        {["stdout", "stderr"].map((t) => (
-          <TabsContent key={t} className="h-full relative" value={t}>
-            <div className="h-[70vh] overflow-y-auto">
-              <pre id={t} className="-scroll-mt-24">
-                {logs?.[t as keyof typeof logs] || `no ${t} logs`}
-              </pre>
-            </div>
-            <Button
-              className="absolute bottom-4 right-4"
-              onClick={to_bottom(t)}
-            >
-              <ChevronDown className="h-4 w-4" />
-            </Button>
-          </TabsContent>
-        ))}
+        {["stdout", "stderr"].map((t) => {
+          const log = logs?.[t as keyof typeof logs] as string | undefined;
+          return (
+            <TabsContent key={t} className="h-full relative" value={t}>
+              <div className="h-[70vh] overflow-y-auto">
+                <pre
+                  id={t}
+                  dangerouslySetInnerHTML={{
+                    __html: log ? logToHtml(log) : `no ${t} logs`,
+                  }}
+                  className="-scroll-mt-24"
+                />
+              </div>
+              <Button
+                className="absolute bottom-4 right-4"
+                onClick={to_bottom(t)}
+              >
+                <ChevronDown className="h-4 w-4" />
+              </Button>
+            </TabsContent>
+          );
+        })}
       </Section>
     </Tabs>
   );
+};
+
+const TailLengthSelector = ({
+  selected,
+  onSelect,
+}: {
+  selected: string;
+  onSelect: (value: string) => void;
+}) => (
+  <Select value={selected} onValueChange={onSelect}>
+    <SelectTrigger>
+      <SelectValue />
+    </SelectTrigger>
+    <SelectContent>
+      <SelectGroup>
+        {["50", "100", "500", "1000"].map((length) => (
+          <SelectItem key={length} value={length}>
+            {length} lines
+          </SelectItem>
+        ))}
+      </SelectGroup>
+    </SelectContent>
+  </Select>
+);
+
+const to_bottom = (id: string) => () =>
+  document
+    .getElementById(id)
+    ?.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+
+const convert = new Convert();
+/**
+ * Converts the ansi colors in log to html.
+ * sanitizes incoming log first for any eg. script tags.
+ * @param log incoming log string
+ */
+const logToHtml = (log: string) => {
+  if (!log) return "No log.";
+  const sanitized = sanitizeHtml(log, {
+    allowedTags: sanitizeHtml.defaults.allowedTags.filter(
+      (tag) => tag !== "script"
+    ),
+    allowedAttributes: sanitizeHtml.defaults.allowedAttributes,
+  });
+  return convert.toHtml(sanitized);
 };
