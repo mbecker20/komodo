79620030bc8eb99d937446ab5a6484c6074b2214
mbecker20 <becker.maxh@gmail.com>
2024-04-07 00:54:04 -0700

    alert / update logger resources

--- bin/alert_logger/src/main.rs
+++ bin/alert_logger/src/main.rs
@@ -6,12 +6,28 @@ use std::{net::SocketAddr, str::FromStr};
 use anyhow::Context;
 use axum::{routing::post, Json, Router};
 use monitor_client::entities::alert::Alert;
+use serde::Deserialize;
 use termination_signal::tokio::immediate_term_handle;
 
+#[derive(Deserialize)]
+struct Env {
+  #[serde(default = "default_port")]
+  port: u16,
+}
+
+fn default_port() -> u16 {
+  7000
+}
+
 async fn app() -> anyhow::Result<()> {
+  dotenv::dotenv().ok();
   logger::init(tracing::Level::INFO);
 
-  let socket_addr = SocketAddr::from_str("0.0.0.0:7000").unwrap();
+  let Env { port } =
+    envy::from_env().context("failed to parse env")?;
+
+  let socket_addr = SocketAddr::from_str(&format!("0.0.0.0:{port}"))
+    .context("invalid socket addr")?;
 
   info!("v {} | {socket_addr}", env!("CARGO_PKG_VERSION"));
 
@@ -25,6 +41,7 @@ async fn app() -> anyhow::Result<()> {
   let listener = tokio::net::TcpListener::bind(socket_addr)
     .await
     .context("failed to bind tcp listener")?;
+  
   axum::serve(listener, app).await.context("server crashed")
 }
 
