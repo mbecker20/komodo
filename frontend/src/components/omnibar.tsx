814e47031dc0cbbfc00ec522556677fa9a759884
mbecker20 <becker.maxh@gmail.com>
2024-05-12 02:55:13 -0700

    dope search

--- frontend/src/components/omnibar.tsx
+++ frontend/src/components/omnibar.tsx
@@ -10,11 +10,9 @@ import {
   CommandItem,
 } from "@ui/command";
 import { Home, Search, UserCircle2 } from "lucide-react";
-import { Fragment } from "react";
+import { Fragment, ReactNode, useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { ResourceComponents } from "./resources";
-import { UsableResource } from "@types";
-import { RESOURCE_TARGETS, cn, usableResourcePath } from "@lib/utils";
+import { cn } from "@lib/utils";
 import { DeploymentComponents } from "./resources/deployment";
 import { BuildComponents } from "./resources/build";
 import { ServerComponents } from "./resources/server";
@@ -45,6 +43,12 @@ export const OmniSearch = ({
   );
 };
 
+type OmniItem = {
+  label: string;
+  icon: ReactNode;
+  onSelect: () => void;
+};
+
 export const OmniDialog = ({
   open,
   setOpen,
@@ -52,111 +56,194 @@ export const OmniDialog = ({
   open: boolean;
   setOpen: (open: boolean) => void;
 }) => {
-  const user = useUser().data;
+  const [search, setSearch] = useState("");
   const navigate = useNavigate();
   const nav = (value: string) => {
     setOpen(false);
     navigate(value);
   };
+  const items = useOmniItems(nav, search);
   return (
-    <CommandDialog open={open} onOpenChange={setOpen}>
-      <CommandInput placeholder="Type a command or search..." />
+    <CommandDialog open={open} onOpenChange={setOpen} manualFilter>
+      <CommandInput
+        placeholder="Type a command or search..."
+        value={search}
+        onValueChange={setSearch}
+      />
       <CommandList>
         <CommandEmpty>No results found.</CommandEmpty>
 
-        <CommandGroup>
-          <CommandItem
-            className="flex items-center gap-2 cursor-pointer"
-            onSelect={() => nav("/")}
-          >
-            <Home className="w-4 h-4" />
-            Home
-          </CommandItem>
-          <CommandItem
-            className="flex items-center gap-2 cursor-pointer"
-            onSelect={() => nav("/servers")}
-          >
-            <ServerComponents.Icon />
-            Servers
-          </CommandItem>
-          <CommandItem
-            className="flex items-center gap-2 cursor-pointer"
-            onSelect={() => nav("/deployments")}
-          >
-            <DeploymentComponents.Icon />
-            Deployments
-          </CommandItem>
-          <CommandItem
-            className="flex items-center gap-2 cursor-pointer"
-            onSelect={() => nav("/builds")}
-          >
-            <BuildComponents.Icon />
-            Builds
-          </CommandItem>
-          <CommandItem
-            className="flex items-center gap-2 cursor-pointer"
-            onSelect={() => nav("/repos")}
-          >
-            <RepoComponents.Icon />
-            Repos
-          </CommandItem>
-          <CommandItem
-            className="flex items-center gap-2 cursor-pointer"
-            onSelect={() => nav("/procedures")}
-          >
-            <ProcedureComponents.Icon />
-            Procedures
-          </CommandItem>
-          {user?.admin && (
-            <CommandItem
-              className="flex items-center gap-2 cursor-pointer"
-              onSelect={() => nav("/users")}
-            >
-              <UserCircle2 className="w-4 h-4" />
-              Users
-            </CommandItem>
-          )}
-        </CommandGroup>
-
-        <CommandSeparator />
-
-        {RESOURCE_TARGETS.map((rt) => (
-          <Fragment key={rt}>
-            <ResourceGroup type={rt} key={rt} onSelect={nav} />
-            <CommandSeparator />
-          </Fragment>
-        ))}
+        {Object.entries(items)
+          .filter(([_, items]) => items.length > 0)
+          .map(([key, items], i) => (
+            <Fragment key={key}>
+              {i !== 0 && <CommandSeparator />}
+              <CommandGroup heading={key ? key : undefined}>
+                {items.map(({ label, icon, onSelect }) => (
+                  <CommandItem
+                    key={label}
+                    className="flex items-center gap-2 cursor-pointer"
+                    onSelect={onSelect}
+                  >
+                    {icon}
+                    {label}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </Fragment>
+          ))}
       </CommandList>
     </CommandDialog>
   );
 };
 
-const ResourceGroup = ({
-  type,
-  onSelect,
-}: {
-  type: UsableResource;
-  onSelect: (path: string) => void;
-}) => {
-  const data = useRead(`List${type}s`, {}).data;
-  const Components = ResourceComponents[type];
+const useOmniItems = (
+  nav: (path: string) => void,
+  search: string
+): Record<string, OmniItem[]> => {
+  const user = useUser().data;
+  const servers = useRead("ListServers", {}).data;
+  const deployments = useRead("ListDeployments", {}).data;
+  const builds = useRead("ListBuilds", {}).data;
+  const repos = useRead("ListRepos", {}).data;
+  const procedures = useRead("ListProcedures", {}).data;
+  const searchTerms = search
+    .toLowerCase()
+    .split(" ")
+    .filter((term) => term);
+  return useMemo(
+    () => ({
+      "": [
+        {
+          label: "Home",
+          icon: <Home className="w-4 h-4" />,
+          onSelect: () => nav("/"),
+        },
+        {
+          label: "Servers",
+          icon: <ServerComponents.Icon />,
+          onSelect: () => nav("/servers"),
+        },
+        {
+          label: "Deployments",
+          icon: <DeploymentComponents.Icon />,
+          onSelect: () => nav("/deployments"),
+        },
+        {
+          label: "Builds",
+          icon: <BuildComponents.Icon />,
+          onSelect: () => nav("/builds"),
+        },
+        {
+          label: "Repos",
+          icon: <RepoComponents.Icon />,
+          onSelect: () => nav("/repos"),
+        },
+        {
+          label: "Procedures",
+          icon: <ProcedureComponents.Icon />,
+          onSelect: () => nav("/procedures"),
+        },
+        (user?.admin && {
+          label: "Users",
+          icon: <UserCircle2 className="w-4 h-4" />,
+          onSelect: () => nav("/users"),
+        }) as OmniItem,
+      ]
+        .filter((item) => item)
+        .filter((item) => {
+          const label = item.label.toLowerCase();
+          return (
+            searchTerms.length === 0 ||
+            searchTerms.every((term) => label.includes(term))
+          );
+        }),
 
-  if (!data || !data.length) return;
+      Servers:
+        servers
+          ?.filter(
+            (item) =>
+              searchTerms.length === 0 ||
+              searchTerms.every(
+                (term) =>
+                  item.name.toLowerCase().includes(term) ||
+                  "server".includes(term)
+              )
+          )
+          .map((server) => ({
+            label: server.name,
+            icon: <ServerComponents.Icon id={server.id} />,
+            onSelect: () => nav(`/servers/${server.id}`),
+          })) || [],
 
-  return (
-    <CommandGroup heading={`${type}s`}>
-      {data?.map(({ id }) => {
-        return (
-          <CommandItem
-            key={id}
-            className="cursor-pointer flex items-center gap-2"
-            onSelect={() => onSelect(`/${usableResourcePath(type)}/${id}`)}
-          >
-            <Components.Icon id={id} />
-            <Components.Name id={id} />
-          </CommandItem>
-        );
-      })}
-    </CommandGroup>
+      Deployments:
+        deployments
+          ?.filter(
+            (item) =>
+              searchTerms.length === 0 ||
+              searchTerms.every(
+                (term) =>
+                  item.name.toLowerCase().includes(term) ||
+                  "deployment".includes(term)
+              )
+          )
+          .map((deployment) => ({
+            label: deployment.name,
+            icon: <DeploymentComponents.Icon id={deployment.id} />,
+            onSelect: () => nav(`/deployments/${deployment.id}`),
+          })) || [],
+
+      Build:
+        builds
+          ?.filter(
+            (item) =>
+              searchTerms.length === 0 ||
+              searchTerms.every(
+                (term) =>
+                  item.name.toLowerCase().includes(term) ||
+                  "build".includes(term)
+              )
+          )
+          .map((build) => ({
+            label: build.name,
+            icon: <BuildComponents.Icon id={build.id} />,
+            onSelect: () => nav(`/builds/${build.id}`),
+          })) || [],
+
+      Repos:
+        repos
+          ?.filter(
+            (item) =>
+              searchTerms.length === 0 ||
+              searchTerms.every(
+                (term) =>
+                  item.name.toLowerCase().includes(term) ||
+                  "repo".includes(term)
+              )
+          )
+          .map((repo) => ({
+            label: repo.name,
+            icon: <RepoComponents.Icon id={repo.id} />,
+            onSelect: () => nav(`/repos/${repo.id}`),
+          })) || [],
+
+      Procedures:
+        procedures
+          ?.filter(
+            (item) =>
+              searchTerms.length === 0 ||
+              searchTerms.every(
+                (term) =>
+                  item.name.toLowerCase().includes(term) ||
+                  "procedure".includes(term)
+              )
+          )
+          .map((procedure) => ({
+            label: procedure.name,
+            icon: <ProcedureComponents.Icon id={procedure.id} />,
+            onSelect: () => nav(`/procedures/${procedure.id}`),
+          })) || [],
+    }),
+    [user, servers, deployments, builds, repos, procedures, search]
   );
 };
