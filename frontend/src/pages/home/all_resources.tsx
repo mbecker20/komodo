da5b71eaf29fb78647e52583ded43f781f6956f8
kv <karamvir.singh98@gmail.com>
2024-08-04 01:17:14 -0700

    cleanup all resources page layout

--- frontend/src/pages/home/all_resources.tsx
+++ frontend/src/pages/home/all_resources.tsx
@@ -3,9 +3,12 @@ import { Page, Section } from "@components/layouts";
 import { ResourceComponents } from "@components/resources";
 import { TagsFilter } from "@components/tags";
 import { useFilterResources, useRead, useTagsFilter } from "@lib/hooks";
+import { cn } from "@lib/utils";
 import { Types } from "@monitor/client";
 import { RequiredResourceComponents, UsableResource } from "@types";
+import { Button } from "@ui/button";
 import { Input } from "@ui/input";
+import { ChevronDown, ChevronUp } from "lucide-react";
 import { useState } from "react";
 
 export const AllResources = () => {
@@ -22,9 +25,9 @@ export const AllResources = () => {
               placeholder="search..."
               className="w-[200px] lg:w-[300px]"
             />
-            <ExportButton tags={tags} />
+            <TagsFilter />
           </div>
-          <TagsFilter />
+          <ExportButton tags={tags} />
         </div>
       }
     >
@@ -58,11 +61,34 @@ const TableSection = ({
 
   let count = filtered.length;
 
+  const [show, setShow] = useState(true);
+
   if (!count) return;
 
   return (
-    <Section key={type} title={type + "s"}>
-      <Components.Table resources={filtered ?? []} />
+    <Section
+      key={type}
+      title={type + "s"}
+      icon={<Components.Icon />}
+      actions={
+        <Button
+          size="sm"
+          variant="outline"
+          className="gap-4"
+          onClick={() => setShow(!show)}
+        >
+          {show ? "Hide" : "Show"}
+          {show ? (
+            <ChevronUp className="w-4" />
+          ) : (
+            <ChevronDown className="w-4" />
+          )}
+        </Button>
+      }
+    >
+      <div className={cn("border-b", show && "pb-16")}>
+        {show && <Components.Table resources={filtered ?? []} />}
+      </div>
     </Section>
   );
 };
