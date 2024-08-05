c889e436b43318bc62b48c94a0a10763edeae61b
kv <karamvir.singh98@gmail.com>
2024-08-05 15:48:25 -0700

    update shadcn components

--- frontend/src/ui/card.tsx
+++ frontend/src/ui/card.tsx
@@ -45,7 +45,7 @@ const CardDescription = React.forwardRef<
   HTMLParagraphElement,
   React.HTMLAttributes<HTMLParagraphElement>
 >(({ className, ...props }, ref) => (
-  <div
+  <p
     ref={ref}
     className={cn("text-sm text-muted-foreground", className)}
     {...props}
