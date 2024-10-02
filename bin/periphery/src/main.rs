b9acec1d2588f1ca0bce463897133d8d5fa4c427
mbecker20 <becker.maxh@gmail.com>
2024-10-02 02:48:46 -0400

    try periphery rustls

--- bin/periphery/src/main.rs
+++ bin/periphery/src/main.rs
@@ -4,7 +4,7 @@ extern crate tracing;
 use std::{net::SocketAddr, str::FromStr};
 
 use anyhow::Context;
-use axum_server::tls_openssl::OpenSSLConfig;
+use axum_server::tls_rustls::RustlsConfig;
 
 mod api;
 mod compose;
@@ -38,12 +38,12 @@ async fn app() -> anyhow::Result<()> {
     info!("🔒 Periphery SSL Enabled");
     ssl::ensure_certs().await;
     info!("Komodo Periphery starting on https://{}", socket_addr);
-    let ssl_config = OpenSSLConfig::from_pem_file(
+    let ssl_config = RustlsConfig::from_pem_file(
       &config.ssl_cert_file,
       &config.ssl_key_file,
     )
-    .context("Failed to parse ssl ")?;
-    axum_server::bind_openssl(socket_addr, ssl_config)
+    .context("Invalid ssl cert / key")?;
+    axum_server::bind_rustls(socket_addr, ssl_config)
       .serve(app)
       .await?
   } else {
