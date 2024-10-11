3da41f543923ba30b29b67043c23381444e0cf4c
mbecker20 <becker.maxh@gmail.com>
2024-10-11 19:47:13 -0400

    1.15.5 - Update your user's username and password - **Admin**: Delete Users

--- client/core/rs/src/api/write/user.rs
+++ client/core/rs/src/api/write/user.rs
@@ -3,9 +3,64 @@ use resolver_api::derive::Request;
 use serde::{Deserialize, Serialize};
 use typeshare::typeshare;
 
-use crate::entities::user::User;
+use crate::entities::{user::User, NoData};
 
 use super::KomodoWriteRequest;
+
+//
+
+/// **Only for local users**. Update the calling users username.
+/// Response: [NoData].
+#[typeshare]
+#[derive(
+  Serialize, Deserialize, Debug, Clone, Request, EmptyTraits,
+)]
+#[empty_traits(KomodoWriteRequest)]
+#[response(UpdateUserUsernameResponse)]
+pub struct UpdateUserUsername {
+  pub username: String,
+}
+
+#[typeshare]
+pub type UpdateUserUsernameResponse = NoData;
+
+//
+
+/// **Only for local users**. Update the calling users password.
+/// Response: [NoData].
+#[typeshare]
+#[derive(
+  Serialize, Deserialize, Debug, Clone, Request, EmptyTraits,
+)]
+#[empty_traits(KomodoWriteRequest)]
+#[response(UpdateUserPasswordResponse)]
+pub struct UpdateUserPassword {
+  pub password: String,
+}
+
+#[typeshare]
+pub type UpdateUserPasswordResponse = NoData;
+
+//
+
+/// **Only for admin**. Delete user.
+/// Admins can delete any non-admin user.
+/// Only Super Admin can delete an admin.
+/// User cannot delete themselves.
+/// Response: [NoData].
+#[typeshare]
+#[derive(
+  Serialize, Deserialize, Debug, Clone, Request, EmptyTraits,
+)]
+#[empty_traits(KomodoWriteRequest)]
+#[response(DeleteUserResponse)]
+pub struct DeleteUser {
+  /// User id or username
+  pub user: String,
+}
+
+#[typeshare]
+pub type DeleteUserResponse = NoData;
 
 //
 
